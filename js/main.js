a61e7e5ae06a28fda842444ef08debd5ed2ae659
kajal <kzlchingsubam@gmail.com>
2018-10-22 05:28:00 +0000

    ux funcionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
-document.addEventListener('click', (el) => {
-	console.log(el.target.offsetHeight - 42);
-})
+/* 
+    UX FUNCTIONALITY
+    =====================================================
+    Note remains in html format until when its edited, 
+    then the html is converted to textarea.
+    
+    The Note is divided into three parts: heading, body
+    and tags. The heading is formatted in yellow color
+    with bold font. The body remains white, and the tags
+    are blue.
+    
+    The heading is a seperate textarea, including each 
+    individual parahs of the body. Everytime the user 
+    hits enter a new textarea must be created and if the
+    textarea is empty and the user hits backspace the
+    textarea is deleted and the focus move to the previous
+    textarea.
+    
+    The tags are updated only if the user uses a "#" in
+    the body. Tags section cannot be edited by the user.
+    
+    Functions:
+    Double click - edit note
+    ====================================================
+*/
+
