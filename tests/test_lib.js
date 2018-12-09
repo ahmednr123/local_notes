c8c67b7ebaf6946c3874c97c1745eec7a4d43e89
Ahmed Noor <ahmednr123@gmail.com>
2018-12-09 21:44:27 +0530

    yolo

---
+++ tests/test_lib.js
@@ -0,0 +1,16 @@
+var _test = {}
+
+function TestEngine () {
+	this.run = function() {
+		
+	}
+}
+
+
+function TestCase (name, data, result, remark) {
+	return {name, data, result, remark}
+}
+
+function Test (name, func) {
+	
+}
